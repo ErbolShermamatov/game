f1777d97e1a32e0f7e4181c55fb2e4014bc7f112
Erbol <erbol.shermamatov7@gmail.com>
2026-06-02 16:25:59 +0200

    feat: add basic player horizontal movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+    private float horizontalInput;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
     }
 }
